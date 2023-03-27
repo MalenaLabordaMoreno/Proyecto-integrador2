256fe0cc781444f5434ad9d26385a80988d560db
MalenaLabordaMoreno <mlabordamoreno@udesa.edu.ar>
2023-03-27 12:09:11 -0300

    rutas y controladores

---
+++ miProyecto/controllers/productosController.js
@@ -0,0 +1,8 @@
+let productorController = {
+    index: function (req,res) {
+        return res.send ('Index de productos')
+    },
+};
+
+
+module.exports = productosController; 
---
+++ miProyecto/routes/productos.js
@@ -0,0 +1,7 @@
+let express = requiere('express');
+let router = express.Router();
+
+// rutas de productos 
+router.get()
+
+module.exports = router;
