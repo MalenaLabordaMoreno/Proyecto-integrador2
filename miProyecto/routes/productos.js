589e0d26ddbd971c6e2bd2396906eae60178a628
MalenaLabordaMoreno <mlabordamoreno@udesa.edu.ar>
2023-03-30 15:48:10 -0300

    carpetas.controlador.rutas

--- miProyecto/routes/productos.js
+++ miProyecto/routes/productos.js
@@ -1,7 +1,8 @@
+const productosController = require ('../controllers/productosController');
 let express = requiere('express');
 let router = express.Router();
 
-// rutas de productos 
-router.get()
+// Rutas de productos 
+router.get('/agregar', productosController.agregar) // http://localhost:3000/productos/agregar
 
 module.exports = router;
