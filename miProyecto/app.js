8520c1894f780a015092454ced84cc8776d55813
MalenaLabordaMoreno <mlabordamoreno@udesa.edu.ar>
2023-03-30 14:57:56 -0300

    app.js

--- miProyecto/app.js
+++ miProyecto/app.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter); // la usamos para home y creditos 
 app.use('/users', usersRouter); 
-app.use('/productos',productosRouter); // ruta troncal. la usamos para productos. 
+app.use('/aproductos',productosRouter); // ruta troncal. la usamos para productos. 
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
