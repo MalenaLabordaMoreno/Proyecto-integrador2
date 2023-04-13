8440fd93920c1c0bbb3a6f60951323a9bcf39db0
MalenaLabordaMoreno <mlabordamoreno@udesa.edu.ar>
2023-04-13 20:02:37 -0300

    sql

---
+++ comentarios.sql
@@ -0,0 +1,46 @@
+/*
+-- Query: SELECT * FROM base_datos.comentarios
+LIMIT 0, 1000
+
+-- Date: 2023-04-13 19:46
+*/
+INSERT INTO `` (`id`,`usuario_id`,`productos_id`,`comentario`,`createdAt`,`updatedAt`,`deletedAt`) VALUES (1,1,1,'Muy buena cafetera!',NULL,NULL,NULL);
+INSERT INTO `` (`id`,`usuario_id`,`productos_id`,`comentario`,`createdAt`,`updatedAt`,`deletedAt`) VALUES (2,1,4,'Excelente servici',NULL,NULL,NULL);
+INSERT INTO `` (`id`,`usuario_id`,`productos_id`,`comentario`,`createdAt`,`updatedAt`,`deletedAt`) VALUES (3,1,2,'Gran calidad de productos',NULL,NULL,NULL);
+INSERT INTO `` (`id`,`usuario_id`,`productos_id`,`comentario`,`createdAt`,`updatedAt`,`deletedAt`) VALUES (4,1,4,'Infaltable para mis mañanas',NULL,NULL,NULL);
+INSERT INTO `` (`id`,`usuario_id`,`productos_id`,`comentario`,`createdAt`,`updatedAt`,`deletedAt`) VALUES (5,1,3,'Gran producto para hacer un regalo',NULL,NULL,NULL);
+INSERT INTO `` (`id`,`usuario_id`,`productos_id`,`comentario`,`createdAt`,`updatedAt`,`deletedAt`) VALUES (6,1,6,'Muy mala calidad',NULL,NULL,NULL);
+INSERT INTO `` (`id`,`usuario_id`,`productos_id`,`comentario`,`createdAt`,`updatedAt`,`deletedAt`) VALUES (7,1,4,'Muy cmoda y practica',NULL,NULL,NULL);
+INSERT INTO `` (`id`,`usuario_id`,`productos_id`,`comentario`,`createdAt`,`updatedAt`,`deletedAt`) VALUES (8,1,9,'Mi hijo la usa todo el tiempo ',NULL,NULL,NULL);
+INSERT INTO `` (`id`,`usuario_id`,`productos_id`,`comentario`,`createdAt`,`updatedAt`,`deletedAt`) VALUES (9,2,7,'Muy facil de lavar y usar',NULL,NULL,NULL);
+INSERT INTO `` (`id`,`usuario_id`,`productos_id`,`comentario`,`createdAt`,`updatedAt`,`deletedAt`) VALUES (10,2,8,'Muy practica para viajes',NULL,NULL,NULL);
+INSERT INTO `` (`id`,`usuario_id`,`productos_id`,`comentario`,`createdAt`,`updatedAt`,`deletedAt`) VALUES (11,2,4,'no puede faltar en mi casa',NULL,NULL,NULL);
+INSERT INTO `` (`id`,`usuario_id`,`productos_id`,`comentario`,`createdAt`,`updatedAt`,`deletedAt`) VALUES (12,2,1,'Me salvo la vida',NULL,NULL,NULL);
+INSERT INTO `` (`id`,`usuario_id`,`productos_id`,`comentario`,`createdAt`,`updatedAt`,`deletedAt`) VALUES (13,2,2,'Buena relacion precioo/calidad',NULL,NULL,NULL);
+INSERT INTO `` (`id`,`usuario_id`,`productos_id`,`comentario`,`createdAt`,`updatedAt`,`deletedAt`) VALUES (14,2,5,'Muy buena atencion de los empleados',NULL,NULL,NULL);
+INSERT INTO `` (`id`,`usuario_id`,`productos_id`,`comentario`,`createdAt`,`updatedAt`,`deletedAt`) VALUES (15,2,3,'Gran compra!',NULL,NULL,NULL);
+INSERT INTO `` (`id`,`usuario_id`,`productos_id`,`comentario`,`createdAt`,`updatedAt`,`deletedAt`) VALUES (16,2,5,'Me facina esta cafetera',NULL,NULL,NULL);
+INSERT INTO `` (`id`,`usuario_id`,`productos_id`,`comentario`,`createdAt`,`updatedAt`,`deletedAt`) VALUES (17,2,4,'Gran variedad de productos ',NULL,NULL,NULL);
+INSERT INTO `` (`id`,`usuario_id`,`productos_id`,`comentario`,`createdAt`,`updatedAt`,`deletedAt`) VALUES (18,3,3,'Muy canchero el local',NULL,NULL,NULL);
+INSERT INTO `` (`id`,`usuario_id`,`productos_id`,`comentario`,`createdAt`,`updatedAt`,`deletedAt`) VALUES (19,3,2,'Muy rico cafe',NULL,NULL,NULL);
+INSERT INTO `` (`id`,`usuario_id`,`productos_id`,`comentario`,`createdAt`,`updatedAt`,`deletedAt`) VALUES (20,3,7,'Gran adquisicion en mi casa',NULL,NULL,NULL);
+INSERT INTO `` (`id`,`usuario_id`,`productos_id`,`comentario`,`createdAt`,`updatedAt`,`deletedAt`) VALUES (21,3,6,'La usd todos los dias!',NULL,NULL,NULL);
+INSERT INTO `` (`id`,`usuario_id`,`productos_id`,`comentario`,`createdAt`,`updatedAt`,`deletedAt`) VALUES (22,3,9,'No quede muy contenta despues de la compra',NULL,NULL,NULL);
+INSERT INTO `` (`id`,`usuario_id`,`productos_id`,`comentario`,`createdAt`,`updatedAt`,`deletedAt`) VALUES (23,3,5,'Me encanto esta cafetera',NULL,NULL,NULL);
+INSERT INTO `` (`id`,`usuario_id`,`productos_id`,`comentario`,`createdAt`,`updatedAt`,`deletedAt`) VALUES (24,3,4,'Excelente calidad de cafe',NULL,NULL,NULL);
+INSERT INTO `` (`id`,`usuario_id`,`productos_id`,`comentario`,`createdAt`,`updatedAt`,`deletedAt`) VALUES (25,3,7,'Gran sabor',NULL,NULL,NULL);
+INSERT INTO `` (`id`,`usuario_id`,`productos_id`,`comentario`,`createdAt`,`updatedAt`,`deletedAt`) VALUES (26,4,4,'Gracias por la atencion',NULL,NULL,NULL);
+INSERT INTO `` (`id`,`usuario_id`,`productos_id`,`comentario`,`createdAt`,`updatedAt`,`deletedAt`) VALUES (27,4,3,'Muy buen servicio',NULL,NULL,NULL);
+INSERT INTO `` (`id`,`usuario_id`,`productos_id`,`comentario`,`createdAt`,`updatedAt`,`deletedAt`) VALUES (28,4,2,'Gran catalogo de productos',NULL,NULL,NULL);
+INSERT INTO `` (`id`,`usuario_id`,`productos_id`,`comentario`,`createdAt`,`updatedAt`,`deletedAt`) VALUES (29,4,1,'Excelente',NULL,NULL,NULL);
+INSERT INTO `` (`id`,`usuario_id`,`productos_id`,`comentario`,`createdAt`,`updatedAt`,`deletedAt`) VALUES (30,4,5,'Muy buen sabor de cafe',NULL,NULL,NULL);
+INSERT INTO `` (`id`,`usuario_id`,`productos_id`,`comentario`,`createdAt`,`updatedAt`,`deletedAt`) VALUES (31,4,4,'Muy buena variedad de productos',NULL,NULL,NULL);
+INSERT INTO `` (`id`,`usuario_id`,`productos_id`,`comentario`,`createdAt`,`updatedAt`,`deletedAt`) VALUES (32,4,5,'Lindisimas maquinas ',NULL,NULL,NULL);
+INSERT INTO `` (`id`,`usuario_id`,`productos_id`,`comentario`,`createdAt`,`updatedAt`,`deletedAt`) VALUES (33,4,6,'Cafe delicioso',NULL,NULL,NULL);
+INSERT INTO `` (`id`,`usuario_id`,`productos_id`,`comentario`,`createdAt`,`updatedAt`,`deletedAt`) VALUES (34,5,7,'La recomiendo!',NULL,NULL,NULL);
+INSERT INTO `` (`id`,`usuario_id`,`productos_id`,`comentario`,`createdAt`,`updatedAt`,`deletedAt`) VALUES (35,5,2,'Se ve genial en mi cocina, comprenla!',NULL,NULL,NULL);
+INSERT INTO `` (`id`,`usuario_id`,`productos_id`,`comentario`,`createdAt`,`updatedAt`,`deletedAt`) VALUES (36,5,9,'Me impresiono ',NULL,NULL,NULL);
+INSERT INTO `` (`id`,`usuario_id`,`productos_id`,`comentario`,`createdAt`,`updatedAt`,`deletedAt`) VALUES (37,5,7,'La cafetera ha cambiado mi rutina ',NULL,NULL,NULL);
+INSERT INTO `` (`id`,`usuario_id`,`productos_id`,`comentario`,`createdAt`,`updatedAt`,`deletedAt`) VALUES (38,5,6,'Diseños excelentes y modernos',NULL,NULL,NULL);
+INSERT INTO `` (`id`,`usuario_id`,`productos_id`,`comentario`,`createdAt`,`updatedAt`,`deletedAt`) VALUES (39,5,5,'Esperaba mas. Insatisfecho',NULL,NULL,NULL);
+INSERT INTO `` (`id`,`usuario_id`,`productos_id`,`comentario`,`createdAt`,`updatedAt`,`deletedAt`) VALUES (40,5,4,'Sabor excepcional ',NULL,NULL,NULL);
---
+++ productos.sql
@@ -0,0 +1,16 @@
+/*
+-- Query: SELECT * FROM base_datos.productos
+LIMIT 0, 1000
+
+-- Date: 2023-04-13 19:45
+*/
+INSERT INTO `` (`id`,`usuario_id`,`nombre_producto`,`descripcion`,`createdAt`,`updatedAt`,`deletedAt`) VALUES (1,1,'Cafetera Verde ','Cafetera Verde',NULL,NULL,NULL);
+INSERT INTO `` (`id`,`usuario_id`,`nombre_producto`,`descripcion`,`createdAt`,`updatedAt`,`deletedAt`) VALUES (2,2,'Cafetera Citiz Chrome','Cafetera Citiz Chrome',NULL,NULL,NULL);
+INSERT INTO `` (`id`,`usuario_id`,`nombre_producto`,`descripcion`,`createdAt`,`updatedAt`,`deletedAt`) VALUES (3,3,'Cafetera Creatista Metal ','Cafetera Creatista Metal',NULL,NULL,NULL);
+INSERT INTO `` (`id`,`usuario_id`,`nombre_producto`,`descripcion`,`createdAt`,`updatedAt`,`deletedAt`) VALUES (4,4,'Cafeteras Latissima White ','Cafetera Latissima White',NULL,NULL,NULL);
+INSERT INTO `` (`id`,`usuario_id`,`nombre_producto`,`descripcion`,`createdAt`,`updatedAt`,`deletedAt`) VALUES (5,5,'Cafetera Latissima White ','Cafetera Latissima Black',NULL,NULL,NULL);
+INSERT INTO `` (`id`,`usuario_id`,`nombre_producto`,`descripcion`,`createdAt`,`updatedAt`,`deletedAt`) VALUES (6,1,'Cafetera Mini Black','Cafetera Mini Black',NULL,NULL,NULL);
+INSERT INTO `` (`id`,`usuario_id`,`nombre_producto`,`descripcion`,`createdAt`,`updatedAt`,`deletedAt`) VALUES (7,2,'Cafetera Mini Red','Cafetera Mini Red',NULL,NULL,NULL);
+INSERT INTO `` (`id`,`usuario_id`,`nombre_producto`,`descripcion`,`createdAt`,`updatedAt`,`deletedAt`) VALUES (8,3,'Cafetera Mini White','Cafetera Mini White',NULL,NULL,NULL);
+INSERT INTO `` (`id`,`usuario_id`,`nombre_producto`,`descripcion`,`createdAt`,`updatedAt`,`deletedAt`) VALUES (9,4,'Cafetera Marron','Cafetera Marron',NULL,NULL,NULL);
+INSERT INTO `` (`id`,`usuario_id`,`nombre_producto`,`descripcion`,`createdAt`,`updatedAt`,`deletedAt`) VALUES (10,5,'Cafetera Violeta ','Cafetera Violeta',NULL,NULL,NULL);
