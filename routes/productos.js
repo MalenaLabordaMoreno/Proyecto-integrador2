a59b1606ad7ac4c8f92f4059f3305e213338362d
Sguntin <sguntin@udesa.edu.ar>
2023-06-08 22:57:53 -0300

    vistas conectadas y rutas ordenadas

--- routes/productos.js
+++ routes/productos.js
@@ -3,9 +3,12 @@ let express = require('express');
 let router = express.Router();
 
 // Rutas de busqueda - completar con campo opcional progra 1
-router.get('/:id', productosController.search);
-// Rutas de productos 
-router.get('/detalle/:id', productosController.detalle) // http://localhost:3000/productos/detalle/
 router.get('/agregar', productosController.agregar) // http://localhost:3000/productos/agregar
+
+router.post('/agregar', productosController.store) // http://localhost:3000/productos/agregar
+
+// Rutas de productos 
+router.get('/:id', productosController.search);
+router.get('/detalle/:id', productosController.detalle) // http://localhost:3000/productos/detalle/
 
 module.exports = router;
