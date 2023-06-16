05f2fd98d113b096d7e3a9a87624352e33d0fc72
Sguntin <sguntin@udesa.edu.ar>
2023-06-16 19:30:39 -0300

    corregimos editrperfil

--- routes/users.js
+++ routes/users.js
@@ -7,13 +7,13 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.get('/editProfile', usuariosController.edit);
 router.get('/login',usuariosController.login)
 router.get('/register', usuariosController.register)
 router.get('/profile/:id', usuariosController.profile) // le agregamos id?
+router.get('/editProfile/:id', usuariosController.edit)
 
 router.post('/register', usuariosController.store); //Guarda al usuario en la base de datos.
 router.post('/login',usuariosController.processLogin) //redirige al login si encuentra al usuario logueado
 router.post('/logout',usuariosController.logout) //redigire al index
-
+router.post('/editProfile/:id', usuariosController.editarPerfil)
 module.exports = router;
